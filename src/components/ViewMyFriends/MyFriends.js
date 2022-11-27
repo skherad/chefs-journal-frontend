1a28150d886622ef08eb9e272d9d1f219908be04
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2022-11-27 11:43:36 +0300

    friend link bug

--- src/components/ViewMyFriends/MyFriends.js
+++ src/components/ViewMyFriends/MyFriends.js
@@ -9,13 +9,13 @@ const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const MyFriends = ({userId, friends}) => {
 
-
+console.log(friends)
   return (
     <section className='friends'>
       <ul className="explore__user-container">
         {friends.map((e) => {
           return(
-          <Link to={`/userProfile/${userId}/${e.id}`}  key={uniqid()}  className="explore__user links">
+          <Link to={`/userProfile/${userId}/${e.friend_id}`}  key={uniqid()}  className="explore__user links">
               <li  className='explore__user-item' style={{backgroundImage: `url(${e.avatar_url})`}}>
                 <div  className='explore__user-title-container'>
                   <p className='explore__user-title'>{e.name}</p>
